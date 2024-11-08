b11e4c7e3d5919463a0d3587a2b559661f981611
mnawawee <mynawawee@gmail.com>
2024-11-08 14:22:52 +0100

    a

--- pages/1_Real_Time_Prediction.py
+++ pages/1_Real_Time_Prediction.py
@@ -48,11 +48,7 @@ webrtc_streamer(
     key="RealTimePrediction",
     video_frame_callback=video_frame_callback,
     rtc_configuration={
-        "iceServers": [
-            { "urls": "stun:stun.l.google.com:19302" }
-                # Google STUN server
-           
-        ]
+         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
     }          
 )
 
--- pages/2_Registration_form.py
+++ pages/2_Registration_form.py
@@ -77,11 +77,7 @@ webrtc_streamer(
     key='registration',
     video_frame_callback=video_callback_func,
     rtc_configuration={
-        "iceServers": [
-             { "urls": "stun:stun.l.google.com:19302" }
-               # STUN server
-          
-        ]
+         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
     }
 )
 
